6a9ad6bc057dc3ac4620d80588490ad3b5e71f1e
Dmytro Prokhorenkov <dprokhorenkov@liveintent.com>
2024-09-24 13:46:25 +0200

    Automated pwd generation

--- src/setup_script.sh
+++ src/setup_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y openbox xorg libnvidia-gl-550-server dbus-x11 gnome-icon-theme libcanberra-gtk-module libcanberra-gtk3-module libgl1-mesa-dri libgl1-mesa-glx libnotify-bin rtkit xserver-xorg-video-nvidia-550-server nvidia-driver-550-server tightvncserver lightdm
+apt-get install -y openbox xorg libnvidia-gl-550-server dbus-x11 gnome-icon-theme libcanberra-gtk-module libcanberra-gtk3-module libgl1-mesa-dri libgl1-mesa-glx libnotify-bin rtkit xserver-xorg-video-nvidia-550-server nvidia-driver-550-server tightvncserver lightdm expect
 apt-get install -y wget software-properties-common
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt-get install -y ./google-chrome-stable_current_amd64.deb
@@ -13,6 +13,19 @@ cat >/usr/share/glvnd/egl_vendor.d/10_nvidia.json <<EOF
     "library_path": "libEGL_nvidia.so.0"
   }
 }
+EOF
+
+vncpwd_prog=/usr/bin/vncpasswd
+mypass="q1w2e3"
+
+/usr/bin/expect <<EOF
+spawn "$vncpwd_prog -f /etc/vncpasswd"
+expect "Password:"
+send "$mypass\r"
+expect "Verify:"
+send "$mypass\r"
+expect eof
+exit
 EOF
 
 wget -q -O- https://packagecloud.io/dcommander/virtualgl/gpgkey | gpg --dearmor >/etc/apt/trusted.gpg.d/VirtualGL.gpg &&
