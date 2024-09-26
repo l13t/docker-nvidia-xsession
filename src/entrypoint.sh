3d37b819fce6c59daf676c2c3c24f0bc588b0731
Dmytro Prokhorenkov <dprokhorenkov@liveintent.com>
2024-09-26 09:40:21 +0200

    rollback entrypoint

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-# X -config /etc/X11/xorg.conf -noreset -nolisten tcp &
-# sleep 5
-# DISPLAY=:0 openbox-session &
-# sleep 1
-# x11vnc -passwd q1w2e3 -forever -shared -rfbport 5900 -display :0 &
-# sleep 1
-# DISPLAY=:0 xterm &
-# DISPLAY=:0 google-chrome --no-sandbox &
-nvidia-smi
-echo "openbox-session &" >~/.xinitrc
-startx
+X -config /etc/X11/xorg.conf -noreset -nolisten tcp &
+sleep 5
+DISPLAY=:0 openbox-session &
+sleep 1
+x11vnc -passwd q1w2e3 -forever -shared -rfbport 5900 -display :0 &
+sleep 1
+DISPLAY=:0 xterm &
+DISPLAY=:0 google-chrome --no-sandbox &
 
 cat /var/log/Xorg.0.log
