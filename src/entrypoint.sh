83d412e77bba44f50f6ca8da09ae7ce087b09591
Dmytro Prokhorenkov <dprokhorenkov@liveintent.com>
2024-09-26 10:18:43 +0200

    Commented chrome in entrypoint

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -9,10 +9,10 @@ sleep 5
 DISPLAY=:0 dbus-launch
 DISPLAY=:0 openbox-session &
 sleep 1
-x11vnc -passwd q1w2e3 -forever -shared -rfbport 5900 -display :0 &
+x11vnc -passwd q1w2e3 -forever -shared -rfbport 5900 -display :0
 sleep 1
-DISPLAY=:0 xterm &
-DISPLAY=:0 google-chrome --no-sandbox &
+# DISPLAY=:0 xterm &
+# DISPLAY=:0 google-chrome --no-sandbox &
 
 cat /var/log/Xorg.0.log
 while true; do
